f122331de3e9ac0723b53549d544018c254d340f
Denys Petryniak <denys.petryniak@gmail.com>
2023-08-14 21:35:42 +0300

    Tidy up

--- app/Http/Resources/CustomEntry.php
+++ app/Http/Resources/CustomEntry.php
@@ -23,119 +23,49 @@ class CustomEntry extends Entry
         return $defaultAugmentedKeys;
     }
 }
-
-// ______________________________________________________________________
-
-// namespace App\Http\Resources;
-
-// use Statamic\Entries\Entry;
-// use Statamic\Facades\Entry as EntryFacade;
-
-// class CustomEntry extends Entry
-// {
-//     public function shallowAugmentedArrayKeys()
-//     {
-//         if ($this->collectionHandle() == 'blog_articles') {
-//             $augmentedKeys = parent::shallowAugmentedArrayKeys();
-
-//             // Check if this entry has author data
-//             if (isset($this->data['author'])) {
-//                 $augmentedAuthorArray = $this->getAugmentedAuthorArray($this->data['author']);
-//                 $augmentedKeys['author'] = $augmentedAuthorArray;
-//                 dd($augmentedKeys['author']);
-//             }
-
-//             // Add other fields
-//             $augmentedKeys = array_merge($augmentedKeys, ['content', 'test_blog_article_field']);
-
-//             return $augmentedKeys;
-//         }
-
-//         return parent::shallowAugmentedArrayKeys();
-//     }
-
-//     protected function getAugmentedAuthorArray($authorIds)
-//     {
-//         $augmentedAuthorArray = [];
-
-//         foreach ($authorIds as $authorId) {
-//             $authorEntry = EntryFacade::find($authorId);
-
-//             if ($authorEntry) {
-//                 // Augment author data keys
-//                 $augmentedAuthorArray[] = [
-//                     'blueprint' => $authorEntry->get('blueprint'),
-//                     'title' => $authorEntry->get('title'),  // Use get('title') instead of title()
-//                     'updated_by' => $authorEntry->get('updated_by'),
-//                     'updated_at' => $authorEntry->get('updated_at'),
-//                     'test_author_field' => $authorEntry->get('test_author_field'),
-//                     'content' => $authorEntry->get('content'),  // Use get('content') instead of content()
-//                 ];
-//             }
-//         }
-
-//         return $augmentedAuthorArray;
-//     }
-// }
-
-//
+// ____________________________________________________________________
 
 // namespace App\Http\Resources;
 
 // use Statamic\Entries\Entry;
-// use Statamic\Facades\Entry as EntryFacade;
-// use Statamic\Data\AbstractAugmented;
-
-// class AugmentedAuthor extends AbstractAugmented
-// {
-//     public function keys()
-//     {
-//         return [
-//             'title',
-//             'test_author_field',
-//             'test_author_image', // If this field exists in the blueprint
-//         ];
-//     }
-// }
+// use Statamic\Facades\Entry as FacadeEntry;
 
 // class CustomEntry extends Entry
 // {
 //     public function shallowAugmentedArrayKeys()
 //     {
 //         $defaultAugmentedKeys = parent::shallowAugmentedArrayKeys();
+//         $collectionHandle = $this->collectionHandle();
 
-//         if ($this->collectionHandle() == 'blog_articles') {
-//             return array_merge($defaultAugmentedKeys, ['author', 'content', 'test_blog_article_field']);
-//         }
+//         $additionalKeys = [
+//             'blog_articles' => ['author', 'content', 'test_blog_article_field', 'test_blog_article_image'],
+//             'authors' => ['content', 'test_author_field', 'test_author_image'],
+//         ];
 
-//         if ($this->collectionHandle() == 'authors') {
-//             return array_merge($defaultAugmentedKeys, ['content', 'test_author_field', 'test_author_image']);
+//         if (array_key_exists($collectionHandle, $additionalKeys)) {
+//             $augmentedKeys = array_merge($defaultAugmentedKeys, $additionalKeys[$collectionHandle]);
+
+//             if ($collectionHandle === 'blog_articles' && in_array('author', $augmentedKeys)) {
+//                 $authorIds = $this->get('author'); // Assuming 'author' field contains author IDs
+
+//                 $authorData = collect($authorIds)->map(function ($authorId) {
+//                     $authorEntry = FacadeEntry::find($authorId);
+//                     if ($authorEntry) {
+//                         return $authorEntry->shallowAugmentedArrayKeys();
+//                     }
+//                     return null;
+//                 })->filter(); // Remove null entries
+
+
+//                 // Insert the augmented author data into the $augmentedKeys array
+//                 array_splice($augmentedKeys, array_search('author', $augmentedKeys), 1, ['author' => $authorData->toArray()]);
+
+//                 dd($augmentedKeys);
+//             }
+
+//             return $augmentedKeys;
 //         }
 
 //         return $defaultAugmentedKeys;
 //     }
-
-//     public function toAugmentedArray($keys = null)
-//     {
-//         $array = parent::toAugmentedArray($keys);
-
-//         if ($this->collectionHandle() == 'blog_articles' && in_array('author', $keys)) {
-//             $authors = $array['author'] ?? [];
-//             $augmentedAuthors = [];
-
-//             foreach ($authors as $authorId) {
-//                 $authorEntry = EntryFacade::find($authorId);
-
-//                 if ($authorEntry) {
-//                     $augmentedAuthor = (new AugmentedAuthor($authorEntry))->all();
-//                     $augmentedAuthors[] = $augmentedAuthor;
-//                 }
-//             }
-
-//             $array['author'] = $augmentedAuthors;
-//         }
-
-//         return $array;
-//     }
 // }
-
--- app/Http/Resources/CustomEntryResource.php
+++ app/Http/Resources/CustomEntryResource.php
@@ -24,19 +24,20 @@ class CustomEntryResource extends EntryResource
     }
 }
 
-// _____________________________________________
-
+// __________________________________________________
 // namespace App\Http\Resources;
 
 // use Statamic\Http\Resources\API\EntryResource;
+// use Statamic\Facades\Entry as FacadeEntry;
 
 // class CustomEntryResource extends EntryResource
 // {
 //     public function toArray($request)
 //     {
 //         $entryArray = parent::toArray($request);
-
 //         $entryArray = $this->processNestedEntries($entryArray);
+
+//         dd($entryArray);
 
 //         return $entryArray;
 //     }
@@ -44,12 +45,18 @@ class CustomEntryResource extends EntryResource
 //     protected function processNestedEntries($entryArray)
 //     {
 //         foreach ($entryArray as $key => &$value) {
-//             if (is_array($value) && array_key_exists('id', $value)) {
-//                 $entry = \Statamic\Facades\Entry::find($value['id']);
+//             if ($value instanceof \Statamic\Fields\Value && $value->fieldtype() === 'entries') {
+//                 $entryIds = $value->raw();
+//                 $entries = [];
 
-//                 if ($entry) {
-//                     $value = $entry->toAugmentedArray();
+//                 foreach ($entryIds as $entryId) {
+//                     $entry = FacadeEntry::find($entryId);
+//                     if ($entry) {
+//                         $entries[] = $entry->shallowAugmentedArrayKeys();
+//                     }
 //                 }
+
+//                 $value = $entries;
 //             } elseif (is_array($value)) {
 //                 $value = $this->processNestedEntries($value);
 //             }
@@ -59,133 +66,3 @@ class CustomEntryResource extends EntryResource
 //     }
 // }
 
-
-// ______________________________________________
-
-// namespace App\Http\Resources;
-
-// use Statamic\Http\Resources\API\EntryResource;
-// use Statamic\Facades\Entry as EntryFacade;
-
-// class CustomEntryResource extends EntryResource
-// {
-//     public function toArray($request)
-//     {
-//         $augmentedArray = parent::toArray($request);
-
-//         return $this->augmentNestedCollections($augmentedArray);
-//     }
-
-//     protected function augmentNestedCollections($array)
-//     {
-//         foreach ($array as &$value) {
-//             if (is_array($value)) {
-//                 foreach ($value as &$nestedValue) {
-//                     if (is_array($nestedValue)) {
-//                         foreach ($nestedValue as &$nestedItem) {
-//                             if (is_string($nestedItem)) {
-//                                 $nestedItem = $this->augmentNestedItem($nestedItem);
-//                             }
-//                         }
-//                     }
-//                 }
-//             }
-//         }
-
-//         return $array;
-//     }
-
-//     protected function augmentNestedItem($itemId)
-//     {
-//         $item = EntryFacade::find($itemId);
-
-//         if ($item) {
-//             $itemResource = new CustomEntryResource($item);
-//             return $itemResource->toArray(request());
-//         }
-
-//         return null;
-//     }
-// }
-
-// ________________________________________________
-// namespace App\Http\Resources;
-
-// use Statamic\Http\Resources\API\EntryResource;
-// use Statamic\Facades\Asset;
-
-// class CustomEntryResource extends EntryResource
-// {
-//     public function toArray($request)
-//     {
-//         $data = parent::toArray($request);
-
-//         if ($this->resource->collectionHandle() === 'blog_articles') {
-//             $augmentedCollection = $this->resource->toAugmentedCollection()->withShallowNesting();
-
-//             $data['author'] = $augmentedCollection->map(function ($item) {
-//                 $authors = [];
-//                 $authorIds = $item->value('author');
-
-//                 foreach ($authorIds as $authorId) {
-//                     $author = Entry::find($authorId);
-//                     if ($author) {
-//                         $authors[] = [
-//                             'id' => $author->id(),
-//                             'title' => $author->value('title'),
-//                             'url' => $author->url(),
-//                             'permalink' => $author->absoluteUrl(),
-//                             'api_url' => route('statamic.api.entries.show', [$author->collectionHandle(), $author->id()]),
-//                             'content' => $author->value('content'),
-//                             'test_author_field' => $author->value('test_author_field'),
-//                             'test_author_image' => [
-//                                 'id' => $author->value('test_author_image'),
-//                                 'url' => $author->value('test_author_image') ? Asset::find($author->value('test_author_image'))->url() : null,
-//                                 'permalink' => $author->value('test_author_image') ? Asset::find($author->value('test_author_image'))->absoluteUrl() : null,
-//                                 'api_url' => $author->value('test_author_image') ? route('statamic.api.assets.show', [$author->value('test_author_image')]) : null,
-//                                 'width' => $author->value('test_author_image') ? Asset::find($author->value('test_author_image'))->width() : null,
-//                                 'height' => $author->value('test_author_image') ? Asset::find($author->value('test_author_image'))->height() : null,
-//                                 'alt' => $author->value('test_author_image') ? Asset::find($author->value('test_author_image'))->alt() : null,
-//                             ],
-//                         ];
-//                     }
-//                 }
-
-//                 $item['author'] = $authors;
-
-//                 return $item->toArray();
-//             });
-
-//             $data['hero_article'] = $data['author'];
-//         }
-
-//         return $data;
-//     }
-// }
-
-// _________________________________________________
-
-// class CustomEntryResource extends EntryResource
-// {
-//     public function toArray($request)
-//     {
-//         return match($this->resource->collectionHandle()) {
-//           'blog_articles' => $this->customBlogArticlesCollection($request),
-//           default => $this->standard($request),
-//         };
-//     }
-
-//     public function standard($request)
-//     {
-//         return parent::toArray($request);
-//     }
-
-//     public function customBlogArticlesCollection($request)
-//     {
-//         return [
-//             ...$this->standard($request),
-//             'author' => $this->resource->value('author'),
-//             'content' => $this->resource->value('content'),
-//         ];
-//     }
-// }
